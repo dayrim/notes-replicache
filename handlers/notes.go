5506377e87ce00c13afb725f4b51e2ee805196c3
Dmitrijs Pavlovs <dmitrijs.pavlovs123@gmail.com>
2024-08-03 23:38:46 +0300

    update

--- handlers/notes.go
+++ handlers/notes.go
@@ -1,7 +1,7 @@
 package handlers
 
 import (
-    "myproject/models"
+    "notes/models"
     "github.com/gofiber/fiber/v2"
     "github.com/jmoiron/sqlx"
 )
@@ -10,6 +10,14 @@ type NoteHandler struct {
     DB *sqlx.DB
 }
 
+// GetNotes godoc
+// @Summary Get all notes
+// @Description Get all notes
+// @Tags notes
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} models.Note
+// @Router /notes [get]
 func (h *NoteHandler) GetNotes(c *fiber.Ctx) error {
     notes := []models.Note{}
     err := h.DB.Select(&notes, "SELECT * FROM notes")
@@ -19,6 +27,15 @@ func (h *NoteHandler) GetNotes(c *fiber.Ctx) error {
     return c.JSON(notes)
 }
 
+// GetNoteByID godoc
+// @Summary Get note by ID
+// @Description Get note by ID
+// @Tags notes
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Note ID"
+// @Success 200 {object} models.Note
+// @Router /notes/{id} [get]
 func (h *NoteHandler) GetNoteByID(c *fiber.Ctx) error {
     id := c.Params("id")
     note := models.Note{}
@@ -29,6 +46,15 @@ func (h *NoteHandler) GetNoteByID(c *fiber.Ctx) error {
     return c.JSON(note)
 }
 
+// CreateNote godoc
+// @Summary Create a new note
+// @Description Create a new note
+// @Tags notes
+// @Accept  json
+// @Produce  json
+// @Param note body models.Note true "New note"
+// @Success 201 {object} models.Note
+// @Router /notes [post]
 func (h *NoteHandler) CreateNote(c *fiber.Ctx) error {
     note := new(models.Note)
     if err := c.BodyParser(note); err != nil {
@@ -42,6 +68,16 @@ func (h *NoteHandler) CreateNote(c *fiber.Ctx) error {
     return c.Status(201).JSON(note)
 }
 
+// UpdateNoteByID godoc
+// @Summary Update note by ID
+// @Description Update note by ID
+// @Tags notes
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Note ID"
+// @Param note body models.Note true "Updated note"
+// @Success 204
+// @Router /notes/{id} [put]
 func (h *NoteHandler) UpdateNoteByID(c *fiber.Ctx) error {
     id := c.Params("id")
     note := new(models.Note)
@@ -56,6 +92,15 @@ func (h *NoteHandler) UpdateNoteByID(c *fiber.Ctx) error {
     return c.SendStatus(204)
 }
 
+// DeleteNoteByID godoc
+// @Summary Delete note by ID
+// @Description Delete note by ID
+// @Tags notes
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Note ID"
+// @Success 204
+// @Router /notes/{id} [delete]
 func (h *NoteHandler) DeleteNoteByID(c *fiber.Ctx) error {
     id := c.Params("id")
     query := "DELETE FROM notes WHERE id=$1"
--- main.go
+++ main.go
@@ -6,8 +6,24 @@ import (
     "notes/database"
     "notes/routes"
     "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/swagger"
+    _ "notes/docs" // This is necessary for swag to find your docs
 )
 
+// @title Fiber Swagger Example API
+// @version 1.0
+// @description This is a sample server for a notes app.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /
 func main() {
     cfg := config.LoadConfig()
 
@@ -19,6 +35,9 @@ func main() {
     app := fiber.New()
 
     routes.SetupRoutes(app, db)
+
+    // Setup Swagger
+    app.Get("/swagger/*", swagger.HandlerDefault)
 
     log.Fatal(app.Listen(":8080"))
 }
